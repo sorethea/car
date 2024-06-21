83cd81527263f89df2b6dac28de75fa832b53bd3
Sorethea OK <sorethea@gmail.com>
2024-06-21 11:12:17 +0700

    first commit

--- src/CarServiceProvider.php
+++ src/CarServiceProvider.php
@@ -8,10 +8,16 @@ class CarServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-
+        if (! app()->configurationIsCached()) {
+            $this->mergeConfigFrom(__DIR__.'/../config/car.php', 'car');
+        }
     }
 
     public function boot(): void
     {
+        $this->publishesMigrations([__DIR__.'/../database/migrations'=>database_path('migrations')],'car-migrations');
+        $this->publishes([
+            __DIR__.'/../config/car.php' => config_path('car.php'),
+        ], 'car-config');
     }
 }
